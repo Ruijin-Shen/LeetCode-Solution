470048b9b35de5ac9116d48f58f3637deaf6a5a8
Ruijin-Shen <ruijinshen@gmail.com>
2023-06-02 20:37:57 +0800

    Solution for 1. Two Sum.

--- TwoSum.java
+++ TwoSum.java
@@ -4,7 +4,7 @@ public class TwoSum {
 }
 
 
-class Solution {
+class Solution1 {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> lookupTable = new HashMap<>();
         for (int i = 0; i < nums.length; ++i) {

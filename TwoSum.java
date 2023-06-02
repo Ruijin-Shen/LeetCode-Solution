fb4ea7dd386850300c415ae824b40c5ff323f11c
Ruijin-Shen <ruijinshen@gmail.com>
2023-06-02 20:33:44 +0800

    Solution for 1. Two Sum.

---
+++ TwoSum.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class TwoSum {
+}
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> lookupTable = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (lookupTable.containsKey(target - nums[i])) {
+                return new int[]{lookupTable.get(target - nums[i]), i};
+            }
+            lookupTable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}

0c92dbf54ae76ed29fb675ac793e0c64318d5d89
Ruijin-Shen <ruijinshen@gmail.com>
2023-06-08 21:30:34 +0800

    Solution for 2717. Semi-Ordered Permutation.

---
+++ SemiOrderedPermutation.java
@@ -0,0 +1,23 @@
+public class SemiOrderedPermutation {
+}
+
+class Solution2717{
+    public int semiOrderedPermutation(int[] nums) {
+        int n = nums.length;
+        int index1 = 0, indexn = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 1){
+                index1 = i;
+            }
+            if(nums[i] == n){
+                indexn = i;
+            }
+        }
+
+        if(index1 < indexn){
+            return index1 + n - indexn - 1;
+        }else{
+            return index1 + n - indexn - 2;
+        }
+    }
+}

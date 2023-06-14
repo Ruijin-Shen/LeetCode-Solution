0244b49abfe58aacded3561df15739e45eadef1c
Ruijin-Shen <ruijinshen@gmail.com>
2023-06-14 16:45:36 +0800

    Solution for 530. Minimum Absolute Difference in BST.

---
+++ MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,24 @@
+public class MinimumAbsoluteDifferenceInBST {
+}
+
+class SolutionTemp {
+    private int prevNode;
+    private int result;
+
+    public int getMinimumDifference(TreeNode root) {
+        this.prevNode = -100000;
+        this.result = 100000;
+
+        traverse(root);
+        return this.result;
+    }
+
+    private void traverse(TreeNode root){
+        if(root == null) return;
+
+        traverse(root.left);
+        result = Math.min(result, root.val - prevNode);
+        prevNode = root.val;
+        traverse(root.right);
+    }
+}

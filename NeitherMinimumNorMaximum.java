f3d7cf1309a125968e44b997a667d08eee7fc60c
Ruijin-Shen <ruijinshen@gmail.com>
2023-06-14 16:51:33 +0800

    Solution for 2733. Neither Minimum nor Maximum.

---
+++ NeitherMinimumNorMaximum.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class NeitherMinimumNorMaximum {
+}
+
+class Solution2733 {
+    public int findNonMinOrMax(int[] nums) {
+        int n = nums.length;
+        if(n <= 2){
+            return -1;
+        }
+
+        int[] sub = new int[]{nums[0], nums[1], nums[2]};
+        Arrays.sort(sub);
+        return sub[1];
+    }
+}

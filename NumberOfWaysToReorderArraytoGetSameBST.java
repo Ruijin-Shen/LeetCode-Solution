b29841a072f72204258686b092efd351baa87477
Ruijin-Shen <ruijinshen@gmail.com>
2023-06-16 16:30:40 +0800

    Solution to 1569. Number of Ways to Reorder Array to Get Same BST.

---
+++ NumberOfWaysToReorderArraytoGetSameBST.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NumberOfWaysToReorderArraytoGetSameBST {
+}
+
+class Solution1659 {
+    private static final Combination combination = new Combination((long)1e9+7, 1000);
+
+
+    public int numOfWays(int[] nums) {
+        List<Integer> numsList = Arrays.stream(nums).boxed().toList();
+        return (int)(helper(numsList) - 1);
+    }
+
+    public long helper(List<Integer> nums){
+        int n = nums.size();
+        if(n <= 2) return 1;
+
+        int root = nums.get(0);
+        LinkedList<Integer> left = new LinkedList<>();
+        LinkedList<Integer> right = new LinkedList<>();
+
+        for(int i : nums){
+            if(i < root){
+                left.add(i);
+            }
+            if(i > root){
+                right.add(i);
+            }
+        }
+        long leftCount = helper(left);
+        long rightCount = helper(right);
+
+
+        long result = ((leftCount * rightCount) % (1000000007) * combination.compute(left.size()+right.size(), left.size())) % (1000000007);
+        return result;
+    }
+}

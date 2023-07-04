f6a19ffab5a16bacefc8c2d881a4982e10f744c8
Ruijin-Shen <ruijinshen@gmail.com>
2023-07-04 11:02:36 +0800

    Solution to 136. Single Number.

---
+++ SingleNumber.java
@@ -0,0 +1,12 @@
+public class SingleNumber {
+}
+
+class Solution136 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i : nums){
+            result = result ^ i;
+        }
+        return result;
+    }
+}

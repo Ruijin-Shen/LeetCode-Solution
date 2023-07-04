5e85f7640aeb348aac125a550d75d19531238968
Ruijin-Shen <ruijinshen@gmail.com>
2023-07-04 17:18:00 +0800

    Solution to 260. Single Number III.

---
+++ SingleNumberIII.java
@@ -0,0 +1,20 @@
+public class SingleNumberIII {
+}
+
+class Solution260 {
+    public int[] singleNumber(int[] nums) {
+        int XOR = 0;
+        for(int num : nums){
+            XOR ^= num;
+        }
+
+        int lowbit = XOR & -XOR;
+
+        int a = 0, b = 0;
+        for(int num: nums){
+            if((num & lowbit) == 0) a ^= num;
+            else b ^= num;
+        }
+        return new int[]{a, b};
+    }
+}

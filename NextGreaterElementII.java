672407fab888b2e315a9a68f484195dca76c44c9
Ruijin-Shen <ruijinshen@gmail.com>
2023-07-06 19:16:05 +0800

    Solution to 503. Next Greater Element II.

--- NextGreaterElementII.java
+++ NextGreaterElementII.java
@@ -1,33 +1,31 @@
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class NextGreaterElementII {
+    public static void main(String[] args) {
+        Solution503MonotoneStack s = new Solution503MonotoneStack();
+        s.nextGreaterElements(new int[]{1,2,1});
+    }
 }
 
 class Solution503MonotoneStack {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] numsCircular = new int[2 * n];
-        int[] result = new int [2 * n];
-        System.arraycopy(nums, 0, numsCircular, 0, n);
-        System.arraycopy(nums, 0, numsCircular, n, n);
+        int[] result = new int[2 * n];
 
-        Deque<Integer> monotoneStack = new LinkedList<>();
+        int[] monotonicStack = new int[2 * n];
+        int stackTop = -1;
         for(int i = 2 * n - 1; i >= 0; i--){
-            int curNum = numsCircular[i];
-            while(!monotoneStack.isEmpty() && curNum > monotoneStack.getFirst()){
-                monotoneStack.removeFirst();
+            int value = nums[i % n];
+            if(stackTop >= 0 && monotonicStack[stackTop] <= value){
+                stackTop--;
             }
-            if(monotoneStack.isEmpty()){
-                result[i] = -1;
-                monotoneStack.addFirst(numsCircular[i]);
-            }else{
-                result[i] = monotoneStack.getFirst();
-                monotoneStack.addFirst(numsCircular[i]);
-            }
+            if(stackTop >= 0) result[i] = monotonicStack[stackTop];
+            else result[i] = -1;
+            monotonicStack[++stackTop] = value;
         }
-
         return Arrays.copyOf(result, n);
     }
 }

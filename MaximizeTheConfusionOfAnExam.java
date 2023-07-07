4930b64138b6a8859e62401b48a6e72f87adc1b0
Ruijin-Shen <ruijinshen@gmail.com>
2023-07-07 12:12:24 +0800

    Solution to 2024. Maximize the Confusion of an Exam.

---
+++ MaximizeTheConfusionOfAnExam.java
@@ -0,0 +1,21 @@
+public class MaximizeTheConfusionOfAnExam {
+}
+
+class Solution2024 {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int n = answerKey.length();
+        int result = 0;
+
+        int Tcount = 0, Fcount = 0;
+        for(int i = 0, j = 0; j < n; j++){
+            if(answerKey.charAt(j) == 'T') Tcount++;
+            else Fcount++;
+            while(Tcount > k && Fcount > k){
+                if(answerKey.charAt(i++) == 'T') Tcount++;
+                else Fcount++;
+            }
+            result = Math.max(result, j - i + 1);
+        }
+        return result;
+    }
+}

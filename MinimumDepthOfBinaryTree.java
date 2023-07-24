6dcac5dc12a1baf4bb672f82dca78353200bbc8b
Ruijin-Shen <ruijinshen@gmail.com>
2023-07-24 16:40:27 +0800

    Solution to 111. Minimum Depth of Binary Tree.

---
+++ MinimumDepthOfBinaryTree.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class MinimumDepthOfBinaryTree {
+}
+
+class Solution111BFS {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+
+        int depth = 1;
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.addLast(root);
+        while(!queue.isEmpty()){
+            int n = queue.size();
+            for(int i = 0; i < n; i++){
+                TreeNode node = queue.removeFirst();
+                if(node.left == null && node.right == null) return depth;
+                if(node.left != null) queue.addLast(node.left);
+                if(node.right != null) queue.addLast(node.right);
+            }
+            depth++;
+        }
+
+        return depth;
+    }
+}

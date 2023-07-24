83bb7db35badb968231efd7a1c1ce93aa77751ba
Ruijin-Shen <ruijinshen@gmail.com>
2023-07-24 11:47:04 +0800

    Solution to 50. Pow(x, n).

---
+++ Pow.java
@@ -0,0 +1,16 @@
+public class Pow {
+}
+
+class Solution50BinaryExponentiation {
+    public double myPow(double x, int n) {
+        double result = 1;
+        long times = Math.abs((long)n);
+        double base = x;
+        while(times != 0){
+            if((times & 1) == 1) result *= base;
+            base *= base;
+            times >>= 1;
+        }
+        return n < 0 ? (1 / result) : result;
+    }
+}

a8a3fd8f1049c0fc5958cba4fc5489648dcfd716
Ruijin-Shen <ruijinshen@gmail.com>
2023-07-24 11:57:36 +0800

    Solution to 69. Sqrt(x).

---
+++ Sqrt.java
@@ -0,0 +1,14 @@
+public class Sqrt {
+}
+
+class Solution69BinarySearch {
+    public int mySqrt(int x) {
+        double left = 0, right = x;
+        while(right - left > 1e-6){
+            double mid = (left + right) / 2;
+            if(mid * mid < x) left = mid;
+            else right = mid;
+        }
+        return (int)right;
+    }
+}

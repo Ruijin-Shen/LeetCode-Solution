6e461edf78dc057db3937179200b50fd7e6f0c5f
Ruijin-Shen <ruijinshen@gmail.com>
2023-07-25 20:02:56 +0800

    Solution to 852. Peak Index in a Mountain Array.

---
+++ PeakIndexInAMountainArray.java
@@ -0,0 +1,15 @@
+public class PeakIndexInAMountainArray {
+}
+
+class Solution852BinarySearch {
+    public int peakIndexInMountainArray(int[] arr) {
+        // Binary Search
+        int left = 1, right = arr.length - 2;
+        while(left < right){
+            int mid = (left + right + 1) >> 1;
+            if(arr[mid - 1] < arr[mid]) left = mid;
+            else right = mid - 1;
+        }
+        return left;
+    }
+}

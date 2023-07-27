2b37d7152549440bc92694e877950bdde48fd84f
Ruijin-Shen <ruijinshen@gmail.com>
2023-07-27 17:31:03 +0800

    Solution to 303. Range Sum Query - Immutable.

---
+++ RangeSumQueryImmutable.java
@@ -0,0 +1,17 @@
+public class RangeSumQueryImmutable {
+}
+
+class NumArray {
+    private int[] prefixSum;
+
+    public NumArray(int[] nums) {
+        prefixSum = new int[nums.length + 1];
+        for(int i = 0; i < nums.length; i++){
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+}

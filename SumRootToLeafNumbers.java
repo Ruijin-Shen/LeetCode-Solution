08b10fb6d60e1369bfffb8deeb4b3f270d100e62
Ruijin-Shen <ruijinshen@gmail.com>
2023-07-31 18:13:07 +0800

    Solution to 129. Sum Root to Leaf Numbers.

---
+++ SumRootToLeafNumbers.java
@@ -0,0 +1,23 @@
+public class SumRootToLeafNumbers {
+}
+
+class Solution129DFS {
+    private int result = 0;
+
+    public int sumNumbers(TreeNode root) {
+        helper(root, 0);
+        return result;
+    }
+
+    private void helper(TreeNode root, int num){
+        num = num * 10 + root.val;
+
+        if(root.left == null && root.right == null){
+            result += num;
+            return;
+        }
+
+        if(root.left != null) helper(root.left, num);
+        if(root.right != null) helper(root.right, num);
+    }
+}

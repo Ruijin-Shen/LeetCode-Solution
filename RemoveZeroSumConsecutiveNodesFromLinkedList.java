50c5282ce6a38b8e848d6c92933e8a2022d52664
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-04 20:38:46 +0800

    Update 1171. Remove Zero Sum Consecutive Nodes from Linked List.

---
+++ RemoveZeroSumConsecutiveNodesFromLinkedList.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class RemoveZeroSumConsecutiveNodesFromLinkedList {
+}
+
+class Solution1171 {
+    public ListNode removeZeroSumSublists(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        Map<Integer, ListNode> seen = new HashMap<>();
+        int prefix = 0;
+        for (ListNode node = dummy; node != null; node = node.next) {
+            prefix += node.val;
+            seen.put(prefix, node);
+        }
+        prefix = 0;
+        for (ListNode node = dummy; node != null; node = node.next) {
+            prefix += node.val;
+            node.next = seen.get(prefix).next;
+        }
+        return dummy.next;
+    }
+}

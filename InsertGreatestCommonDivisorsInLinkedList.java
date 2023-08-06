c5b55fa1b17cd1b04a8568bc2dbe8bfb9e637874
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-06 20:08:07 +0800

    Solution to 2807. Insert Greatest Common Divisors in Linked List.

---
+++ InsertGreatestCommonDivisorsInLinkedList.java
@@ -0,0 +1,17 @@
+public class InsertGreatestCommonDivisorsInLinkedList {
+}
+
+class Solution2807 {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        for(ListNode node = head; node.next != null; node = node.next.next){
+            int a = node.val, b = node.next.val;
+            node.next = new ListNode(gcd(a, b), node.next);
+        }
+        return head;
+    }
+
+    private static int gcd(int a, int b){
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+}

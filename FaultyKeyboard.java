706c24dc003b2f40d9ea974a1ba014f3bb212120
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-06 20:33:36 +0800

    Solution to 2810. Faulty Keyboard.

---
+++ FaultyKeyboard.java
@@ -0,0 +1,18 @@
+public class FaultyKeyboard {
+}
+
+class Solution2801 {
+    public String finalString(String s) {
+        StringBuilder builder = new StringBuilder();
+        for(char c : s.toCharArray()){
+            switch (c){
+                case 'i':
+                    builder.reverse();
+                    break;
+                default:
+                    builder.append(c);
+            }
+        }
+        return builder.toString();
+    }
+}

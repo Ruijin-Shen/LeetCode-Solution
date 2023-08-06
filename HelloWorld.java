d106c054467b38ecb8da6291c3a1f48f9d1529fd
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-06 21:04:42 +0800

    HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class HelloWorld {
     public static void main(String[] args){
+        int[] a = new int[]{1,3};
         System.out.println("Hello World!"); // Hello World!
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(in);
     }
 }
 
@@ -12,3 +19,15 @@ class ListNode {
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  }
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

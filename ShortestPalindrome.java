b314acc9ec648bbd918f669944b8483ec43423be
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-23 23:25:58 -0400

    Update 214. Shortest Palindrome.

---
+++ ShortestPalindrome.java
@@ -0,0 +1,34 @@
+public class ShortestPalindrome {
+}
+
+class Solution214KMP {
+    public String shortestPalindrome(String s) {
+        String reverse = new StringBuilder(s).reverse().toString();
+        int n = s.length();
+        int[] next = getNext(s);
+
+        int i, j;
+        for(i = 0, j = 0; i < n; i++){
+            while(j > 0 && reverse.charAt(i) != s.charAt(j)) j = next[j - 1];
+            if(reverse.charAt(i) == s.charAt(j)){
+                j++;
+            }
+        }
+
+        StringBuilder builder = new StringBuilder(reverse);
+        builder.append(s.substring(j));
+        return builder.toString();
+    }
+
+    private int[] getNext(String origin){
+        int n = origin.length();
+        int[] next = new int[n];
+
+        for(int i = 1, j = 0; i < n; i++){
+            while(j > 0 && origin.charAt(i) != origin.charAt(j)) j = next[j - 1];
+            if(origin.charAt(i) == origin.charAt(j)) j++;
+            next[i] = j;
+        }
+        return next;
+    }
+}

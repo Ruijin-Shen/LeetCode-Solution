0b4b61c1844d8d2c3c93b54bb671b9eec4511388
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-24 11:20:36 -0400

    Update 858. Mirror Reflection.

---
+++ MirrorReflection.java
@@ -0,0 +1,20 @@
+public class MirrorReflection {
+}
+
+class Solution858 {
+    public int mirrorReflection(int p, int q) {
+        int lcm = LCM(p, q);
+
+        if(lcm / p % 2 == 0) return 0;
+        return lcm / q % 2 == 0 ? 2 : 1;
+    }
+
+    private static int GCD(int a, int b){
+        if(b == 0) return a;
+        return GCD(b, a % b);
+    }
+
+    private static int LCM(int a, int b){
+        return a * b / GCD(a, b);
+    }
+}

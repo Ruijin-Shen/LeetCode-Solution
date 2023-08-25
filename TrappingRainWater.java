5e307714b1ff625b999c016c87b49b201df7b7d8
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-25 17:01:04 -0400

    Solution to 42. Trapping Rain Water.

---
+++ TrappingRainWater.java
@@ -0,0 +1,25 @@
+public class TrappingRainWater {
+}
+
+class Solution42 {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] greater = new int[n];
+
+        int maxNum = 0;
+        for(int i = 0; i < n; i++){
+            if(height[i] > maxNum) maxNum = height[i];
+            greater[i] = maxNum;
+        }
+
+        int result = 0;
+        maxNum = 0;
+        for(int i = n - 1; i >= 0; i--){
+            if(height[i] > maxNum) maxNum = height[i];
+            greater[i] = Math.min(greater[i], maxNum);
+            result += greater[i] - height[i];
+        }
+
+        return result;
+    }
+}

e5ce879d0d48f906eb7541a0a2c96ce22fc83b08
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-25 15:49:44 -0400

    Solution to 97. Interleaving String.

---
+++ InterleavingString.java
@@ -0,0 +1,25 @@
+public class InterleavingString {
+}
+
+class Solution97DynamicProgramming {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length(), n = s2.length();
+        if(m + n != s3.length()) return false;
+        if(s3.length() == 0) return true;
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                if(i - 1 >= 0 && s3.charAt(i + j - 1) == s1.charAt(i - 1)){
+                    dp[i][j] |= dp[i - 1][j];
+                }
+                if(j - 1 >= 0 && s3.charAt(i + j - 1) == s2.charAt(j - 1)){
+                    dp[i][j] |= dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}

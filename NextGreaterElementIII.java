50a916e342e2f6d5a59a0b25d1c09056836d3916
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-27 16:45:11 -0400

    Solution to 556. Next Greater Element III.

---
+++ NextGreaterElementIII.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class NextGreaterElementIII {
+}
+
+class Solution556 {
+    public int nextGreaterElement(int n) {
+        char[] array = Integer.toString(n).toCharArray();
+        int length = array.length;
+
+        int prev = 0;
+        for(int i = length - 1; i >= 0; i--){
+            if(array[i] < prev){
+                char curNum = array[i];
+                Arrays.sort(array, i + 1, length);
+                int left = i + 1, right = length - 1;
+                while(left < right){
+                    int mid = left + right >> 1;
+                    if(array[mid] <= curNum) left = mid + 1;
+                    else right = mid;
+                }
+                char temp = array[right];
+                array[right] = (char)curNum;
+                array[i] = temp;
+                break;
+            }else{
+                prev = array[i];
+            }
+        }
+        long result = Long.parseLong(String.valueOf(array));
+        if(result == n || result > Integer.MAX_VALUE) return -1;
+        else return (int)result;
+    }
+}

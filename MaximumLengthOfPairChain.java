a3b3a9eb2186b0c6a32fec6967ca3fe29989894d
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-27 15:44:02 -0400

    Solution to 646. Maximum Length of Pair Chain.

---
+++ MaximumLengthOfPairChain.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class MaximumLengthOfPairChain {
+}
+
+class Solution646 {
+    public int findLongestChain(int[][] pairs) {
+        int endNum = Integer.MIN_VALUE;
+        int result = 0;
+        Arrays.sort(pairs, (x, y) -> x[1] - y[1]);
+
+        for(int[] pair : pairs){
+            if(pair[0] > endNum){
+                result++;
+                endNum = pair[1];
+            }
+        }
+        return result;
+    }
+}

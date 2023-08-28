ce10b0b8d6cbd5c41ae6a28fcf5eecd32337167d
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-28 11:15:45 -0400

    Solution to 225. Implement Stack using Queues.

---
+++ ImplementStackUsingQueues.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ImplementStackUsingQueues {
+}
+
+class MyStack225 {
+    private Queue<Integer> queue;
+
+    public MyStack225() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue.offer(x);
+        int size = queue.size();
+        for(int i = 0; i < size - 1; i++){
+            queue.offer(queue.poll());
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}

9b59d7f6b174db7b495ac5d0d775925de3310028
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-28 11:59:39 -0400

    Solution to 27. Remove Element.

---
+++ RemoveElement.java
@@ -0,0 +1,17 @@
+public class RemoveElement {
+}
+
+class Solution27 {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        while(left <= right){
+            if(nums[left] == val){
+                nums[left] = nums[right--];
+            }else{
+                left++;
+            }
+        }
+        return right + 1;
+    }
+}

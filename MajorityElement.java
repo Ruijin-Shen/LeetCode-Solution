86f252982a3ce01cceeb125743e84a2dc0abadd8
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-29 12:09:56 -0400

    Solution to 169. Majority Element.

---
+++ MajorityElement.java
@@ -0,0 +1,15 @@
+public class MajorityElement {
+}
+
+class Solution169 {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int count = 0, candidate = nums[0];
+        for(int i = 0; i < n; i++){
+            if(count == 0) candidate = nums[i];
+            if(nums[i] == candidate) count++;
+            else count--;
+        }
+        return candidate;
+    }
+}

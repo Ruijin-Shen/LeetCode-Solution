722b2e1e544a3a5c2351f16e423181df7e639fcc
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-29 10:38:25 -0400

    Solution to 80. Remove Duplicates from Sorted Array II.

---
+++ RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,13 @@
+public class RemoveDuplicatesFromSortedArrayII {
+}
+
+class Solution80 {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int slow, fast;
+        for(slow = 2, fast = 2; fast < n; fast++){
+            if(nums[fast] != nums[slow - 2]) nums[slow++] = nums[fast];
+        }
+        return Math.min(n, slow);
+    }
+}

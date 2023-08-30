1449da606ca954f670d0296fe3126b1648f94da6
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-30 11:38:16 -0400

    Solution to 55. Jump Game.

---
+++ JumpGame.java
@@ -0,0 +1,15 @@
+public class JumpGame {
+}
+
+class Solution55 {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int maxDistance = 0;
+
+        for(int i = 0; i < n && i <= maxDistance; i++){
+            maxDistance = Math.max(maxDistance, i + nums[i]);
+        }
+
+        return maxDistance >= n - 1;
+    }
+}

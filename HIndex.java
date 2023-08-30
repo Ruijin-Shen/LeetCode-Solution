0f385fea8ec33a56ce4f8618568879a56f425f8f
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-30 15:57:20 -0400

    Solution to 274. H-Index.

---
+++ HIndex.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class HIndex {
+}
+
+class Solution274 {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        Arrays.sort(citations);
+        int result = 0;
+        for(int i = n - 1; i >= 0; i--){
+            if(citations[i] >= result + 1){
+                result++;
+            }else{
+                break;
+            }
+        }
+        return result;
+    }
+}

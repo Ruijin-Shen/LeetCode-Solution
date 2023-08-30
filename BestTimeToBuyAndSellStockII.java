808b5134d4bc93315efe22b9dc46489db5f9be36
Ruijin-Shen <ruijinshen@gmail.com>
2023-08-30 11:04:27 -0400

    Solution to 122. Best Time to Buy and Sell Stock II.

---
+++ BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,23 @@
+public class BestTimeToBuyAndSellStockII {
+}
+
+class Solution122 {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        boolean hold = false;
+        int result = 0;
+
+        for(int i = 0; i < n - 1; i++){
+            if(prices[i + 1] > prices[i] && !hold){
+                hold = true;
+                result -= prices[i];
+            }else if(prices[i + 1] <= prices[i] && hold){
+                hold = false;
+                result += prices[i];
+            }
+        }
+        if(hold) result += prices[n - 1];
+
+        return result;
+    }
+}

93733de4cedc9fede7bd028acb1ad65389c72aaf
Ruijin-Shen <ruijinshen@gmail.com>
2023-09-24 17:24:20 -0400

    Solution to 167. Two Sum II - Input Array Is Sorted.

---
+++ TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,11 @@
+class Solution167 {
+    public int[] twoSum(int[] numbers, int target) {
+        for(int i = 0, j = numbers.length - 1; i < j;){
+            int sum = numbers[i] + numbers[j];
+            if(sum == target) return new int[]{i + 1, j + 1};
+            else if(sum > target) j--;
+            else i++;
+        }
+        return null;
+    }
+}

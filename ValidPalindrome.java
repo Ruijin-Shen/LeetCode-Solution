3c0ce1d0b48b4cb044b56f34f8ba35566e1b7b5c
Ruijin-Shen <ruijinshen@gmail.com>
2023-09-24 17:23:43 -0400

    Solution to 125. Valid Palindrome.

---
+++ ValidPalindrome.java
@@ -0,0 +1,21 @@
+public class ValidPalindrome {
+}
+
+class Solution125 {
+    public boolean isPalindrome(String s) {
+        for(int i = 0, j = s.length() - 1; i < j; i++, j--){
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while(i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
+        }
+        return true;
+    }
+}
+
+class Solution125Stream {
+    public boolean isPalindrome(String s) {
+        StringBuilder builder = new StringBuilder();
+        s.chars().filter(c -> Character.isLetterOrDigit(c)).mapToObj(c -> Character.toLowerCase((char) c)).forEach(builder::append);
+        return builder.toString().equals(builder.reverse().toString());
+    }
+}

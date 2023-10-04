c1de805b6f0d06f8e422da5b63ad893e7c29c075
Ruijin-Shen <ruijinshen@gmail.com>
2023-10-04 16:54:10 -0400

    Template for KMP.

--- Template/KMP.java
+++ Template/KMP.java
@@ -1,5 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
+package Template;
+
+import java.util.*;
 
 public class KMP {
     private static int[] next;
@@ -18,11 +19,17 @@ public class KMP {
         }
     }
 
+    /**
+     * Time complexity: O(m) preprocessing + O(n) matching
+     * @param origin
+     * @param pattern
+     * @return the index of patttern in origin
+     */
     public static List<Integer> kmp(String origin, String pattern){
         getNext(pattern);
         int m = origin.length(), n = pattern.length();
         List<Integer> result = new ArrayList<>();
-        // 字符串匹配
+        // String matching
         for(int i = 0, j = 0; i < m; i++){
             while(j > 0 && origin.charAt(i) != pattern.charAt(j)){
                 j = next[j - 1];
@@ -30,7 +37,7 @@ public class KMP {
             if(origin.charAt(i) == pattern.charAt(j)) {
                 j++;
             }
-            if(j == n){ // 匹配成功
+            if(j == n){ // Matching successful!
                 j = next[j - 1];
                 result.add(i - n + 1);
             }

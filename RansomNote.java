c7860c769a4dc9aa5e93b92dbace0a014b284d46
Ruijin-Shen <ruijinshen@gmail.com>
2023-11-06 19:35:16 -0500

    Solution to 383. Ransom Note.

---
+++ RansomNote.java
@@ -0,0 +1,20 @@
+public class RansomNote {
+}
+
+class Solution383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for(char ch : magazine.toCharArray()){
+            count[ch - 'a']++;
+        }
+        for(char ch : ransomNote.toCharArray()){
+            count[ch - 'a']--;
+        }
+
+        for(int i : count){
+            if(i < 0) return false;
+        }
+        return true;
+    }
+}

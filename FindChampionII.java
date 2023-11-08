6d85b64712854e817e72e8fd31b3cc1be3a758de
Ruijin-Shen <ruijinshen@gmail.com>
2023-11-08 17:34:18 -0500

    Solution to 2924. Find Champion II.

---
+++ FindChampionII.java
@@ -0,0 +1,21 @@
+public class FindChampionII {
+}
+
+class Solution2924 {
+    public int findChampion(int n, int[][] edges) {
+        int m = edges.length;
+        int[] indegree = new int[n];
+        for(int i = 0; i < m; i++){
+            indegree[edges[i][1]]++;
+        }
+
+        int result = -1;
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                if(result >= 0) return -1;
+                result = i;
+            }
+        }
+        return result;
+    }
+}

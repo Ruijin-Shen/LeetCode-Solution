fb7e30ddcfb2d4ca649de40f3d72be559bd418f8
Ruijin-Shen <ruijinshen@gmail.com>
2023-11-08 10:58:28 -0500

    Solution to 100. Same Tree.

---
+++ SameTree.java
@@ -0,0 +1,12 @@
+public class SameTree {
+}
+
+class Solution100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == q) return true;  // including null.
+        if(p == null || q == null) return false;
+
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}

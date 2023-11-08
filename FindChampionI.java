676179585e45e168e78c812fadf1ab8574a6d235
Ruijin-Shen <ruijinshen@gmail.com>
2023-11-08 17:15:56 -0500

    Solution to 2923. Find Champion I.

---
+++ FindChampionI.java
@@ -0,0 +1,19 @@
+public class FindChampionI {
+}
+
+class Solution2923 {
+    public int findChampion(int[][] grid) {
+        int n = grid.length;
+        for(int j = 0; j < n; j++){
+            boolean flag = true;
+            for(int i = 0; i < n; i++){
+                if(grid[i][j] == 1){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) return j;
+        }
+        return -1;
+    }
+}

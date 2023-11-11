f9292e94c8d10a914d50f885ad07427371198690
Ruijin-Shen <ruijinshen@gmail.com>
2023-11-11 12:03:49 -0500

    Solution to 102. Binary Tree Level Order Traversal.

---
+++ BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class BinaryTreeLevelOrderTraversal {
+}
+
+class Solution102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result;
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> layer = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.removeFirst();
+                layer.add(node.val);
+                if(node.left != null) queue.addLast(node.left);
+                if(node.right != null) queue.addLast(node.right);
+            }
+            result.add(layer);
+        }
+        return result;
+    }
+}

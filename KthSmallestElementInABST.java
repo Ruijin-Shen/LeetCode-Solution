453608b632cff17b3ffc0db44246b3645baa9b21
Ruijin-Shen <ruijinshen@gmail.com>
2023-11-11 14:09:56 -0500

    Solution to 230. Kth Smallest Element in a BST.

--- KthSmallestElementInABST.java
+++ KthSmallestElementInABST.java
@@ -1,10 +1,12 @@
+import java.util.*;
+
 public class KthSmallestElementInABST {
 }
 
 class Solution230 {
     private int result;
 
-    public int kthSmallest(TreeNode root, int k) {
+    public int kthSmallest230Recursion(TreeNode root, int k) {
         DFS(root, k, 0);
         return result;
     }
@@ -19,5 +21,22 @@ class Solution230 {
         }
         int right = DFS(root.right, k, count + left + 1);
         return left + 1 + right;
+    }
+}
+
+class Solution230Stack {
+    private int result;
+
+    public int kthSmallest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        while(true){
+            while(root != null){
+                stack.addLast(root);
+                root = root.left;
+            }
+            root = stack.removeLast();
+            if(--k == 0) return root.val;
+            root = root.right;
+        }
     }
 }

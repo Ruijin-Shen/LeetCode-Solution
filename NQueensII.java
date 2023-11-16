e34c10a94f811e50f16dbb7453ce42a8ae8c1262
Ruijin-Shen <ruijinshen@gmail.com>
2023-11-15 23:39:04 -0500

    Solution to 52. N-Queens II.

---
+++ NQueensII.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class NQueensII {
+}
+
+class Solution52 {
+    private int size;
+    private Set<Integer> diag;
+    private Set<Integer> antiDiag;
+    private Set<Integer> col;
+
+    public int totalNQueens(int n) {
+        size = n;
+        diag = new HashSet<>();
+        antiDiag = new HashSet<>();
+        col = new HashSet<>();
+        return backtrack(0);
+    }
+
+    private int backtrack(int row){
+        if(row == size) return 1;
+
+        int result = 0;
+        for(int y = 0; y < size; y++){
+            int curDiag = y - row;
+            int curAntiDiag = row + y;
+            if(col.contains(y) || diag.contains(curDiag) || antiDiag.contains(curAntiDiag)){
+                continue;
+            }
+
+            col.add(y);
+            diag.add(curDiag);
+            antiDiag.add(curAntiDiag);
+
+            result += backtrack(row + 1);
+
+            col.remove(y);
+            diag.remove(curDiag);
+            antiDiag.remove(curAntiDiag);
+        }
+        return result;
+    }
+}

7f0a30890448699932ebbba5846c5a8498614cdb
Ruijin-Shen <ruijinshen@gmail.com>
2023-11-22 13:57:01 -0500

    Solution to 82. Remove Duplicates from Sorted List II.

---
+++ RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,22 @@
+public class RemoveDuplicatesFromSortedListII {
+}
+
+class Solution82 {
+    public ListNode deleteDuplicates(ListNode head) {
+        int prev = Integer.MIN_VALUE;
+        ListNode prevNode = new ListNode(0, head);
+        ListNode result = prevNode;
+
+        ListNode node = head;
+        while(node != null){
+            if(node.val == prev || (node.next != null && node.next.val == node.val)){
+                prevNode.next = node.next;
+            }else{
+                prevNode = node;
+            }
+            prev = node.val;
+            node = node.next;
+        }
+        return result.next;
+    }
+}

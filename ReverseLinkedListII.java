93ead908fa78ac884bb61c8f89928095f4999252
Ruijin-Shen <ruijinshen@gmail.com>
2023-11-22 13:38:02 -0500

    Solution to 92. Reverse Linked List II.

---
+++ ReverseLinkedListII.java
@@ -0,0 +1,28 @@
+public class ReverseLinkedListII {
+}
+
+class Solution92 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left == right) return head;
+
+        ListNode node = head;
+        ListNode prev = null;
+        for(int i = 1; i < left; i++){
+            prev = node;
+            node = node.next;
+        }
+        ListNode start = prev;
+        ListNode end = node;
+
+        for(int i = left; i <= right; i++){
+            ListNode temp = node.next;
+            node.next = prev;
+            prev = node;
+            node = temp;
+        }
+
+        if(start != null) start.next = prev;
+        end.next = node;
+        return start == null ? prev: head;
+    }
+}

363b8005ef1478ba3f1c0b9c3fc3b9b59ed1099f
Ruijin-Shen <ruijinshen@gmail.com>
2024-01-15 12:36:55 -0500

    Solution to 3006. Find Beautiful Indices in the Given Array I.

---
+++ Solution/F/FindBeautifulIndicesInTheGivenArrayI.java
@@ -0,0 +1,31 @@
+package Solution.F;
+
+import Template.KMP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindBeautifulIndicesInTheGivenArrayI {
+}
+
+class Solution3006 {
+    public List<Integer> beautifulIndices(String s, String a, String b, int k) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> aIndex = KMP.kmp(s, a);
+        List<Integer> bIndex = KMP.kmp(s, b);
+        if(bIndex.size() == 0) return result;
+
+        for(int i : aIndex){
+            int min = i - k;
+            // binary search
+            int left = 0, right = bIndex.size() -1;
+            while(left < right){
+                int mid = left + right >> 1;
+                if(bIndex.get(mid) < min) left = mid + 1;
+                else right = mid;
+            }
+            if(Math.abs(bIndex.get(left) - i) <= k) result.add(i);
+        }
+        return result;
+    }
+}

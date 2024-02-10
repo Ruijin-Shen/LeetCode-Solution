150cc64077f8e3fb5908fb8c1bf9acb2b0fbbc48
Ruijin-Shen <ruijinshen@gmail.com>
2024-02-10 18:28:00 -0500

    Solution to 493. Reverse Pairs.

--- Solution/R/ReversePairs.java
+++ Solution/R/ReversePairs.java
@@ -4,47 +4,40 @@ public class ReversePairs {
 }
 
 class Solution493 {
-    private static final int[] temp = new int[50000];
+    private static int[] temp = new int[50000];
 
     public int reversePairs(int[] nums) {
-        return (int)countPairs(nums, 0, nums.length - 1);
+        return reversePairs(nums, 0, nums.length - 1);
     }
 
-    private static long countPairs(int[] nums, int left, int right){
+    private static int reversePairs(int[] nums, int left, int right){
         if(left >= right) return 0;
         int mid = left + right >> 1;
-        long intraCount = countPairs(nums, left, mid) + countPairs(nums, mid + 1, right);
-        long interCount = 0;
 
-        // calculate
-        int i = left, j = mid + 1;
+        int result = 0;
+        result += reversePairs(nums, left, mid);
+        result += reversePairs(nums, mid + 1, right);
+
+        int i = left, j = mid + 1, total = left;
+        int ptr = mid + 1;
         while(i <= mid && j <= right){
-            long leftNum = nums[i], rightNum = 2L * nums[j];
-            if(leftNum <= rightNum){
-                i++;
-            }else{
-                interCount += mid - i + 1;
-                j++;
+            if(nums[i] <= nums[j]){
+                while(ptr <= right && (long)nums[ptr] * 2 < nums[i]) ptr++;
+                result += ptr - mid - 1;
+                temp[total++] = nums[i++];
             }
+            else temp[total++] = nums[j++];
         }
-
-        // mergeSort
-        i = left;
-        j = mid + 1;
-        int k = left;
-
-        while(i <= mid && j <= right){
-            if(nums[i] <= nums[j]) temp[k++] = nums[i++];
-            else temp[k++] =  nums[j++];
+        while(i <= mid){
+            while(ptr <= right && (long)nums[ptr] * 2 < nums[i]) ptr++;
+            result += ptr - mid - 1;
+            temp[total++] = nums[i++];
         }
-
-        while(i <= mid) temp[k++] = nums[i++];
-        while(j <= right) temp[k++] = nums[j++];
+        while(j <= right) temp[total++] = nums[j++];
 
         for(i = left; i <= right; i++){
             nums[i] = temp[i];
         }
-
-        return interCount + intraCount;
+        return result;
     }
 }

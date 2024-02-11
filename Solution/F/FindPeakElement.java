d0bfea9e44eecce5baea53f6ba4b36ac1ea362b1
Ruijin-Shen <ruijinshen@gmail.com>
2024-02-10 19:22:10 -0500

    Solution to 162. Find Peak Element.

--- Solution/F/FindPeakElement.java
+++ Solution/F/FindPeakElement.java
@@ -5,16 +5,12 @@ public class FindPeakElement {
 
 class Solution162 {
     public int findPeakElement(int[] nums) {
-        int n = nums.length;
-        int left = -1, right = n - 1;
-        while(right - left > 1){ // left upwards, right downwards.
+        int left = 0, right = nums.length - 1;
+        while(left < right){
             int mid = left + right >> 1;
-            if(mid == n - 1 || nums[mid] > nums[mid + 1]){ // downwards
-                right = mid;
-            }else{
-                left = mid;
-            }
+            if(nums[mid] < nums[mid + 1]) left = mid + 1;
+            else right = mid;
         }
-        return right;
+        return left;
     }
 }

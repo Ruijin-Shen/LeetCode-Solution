ba32b8f0f5702ef490e398b1b0507ad0aa1af482
Ruijin-Shen <ruijinshen@gmail.com>
2024-06-18 00:07:46 +0800

    Solution to 183. Customers Who Never Order.

--- SQL/CustomersWhoNeverOrder.sql
+++ SQL/CustomersWhoNeverOrder.sql
@@ -1,3 +1,7 @@
-SELECT name as 'Customers'
+SELECT name AS 'Customers'
 FROM Customers
 WHERE Customers.id NOT IN (SELECT DISTINCT customerId FROM ORDERS);
+
+SELECT name as Customers
+FROM Customers LEFT JOIN Orders ON Customers.id = Orders.customerId
+WHERE Orders.id IS NULL;

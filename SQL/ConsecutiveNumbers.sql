15f19a3cd0bb02b56a739f0f2b30e715692b5bd2
Ruijin-Shen <ruijinshen@gmail.com>
2024-06-17 23:52:49 +0800

    Solution to 180. Consecutive Numbers.

--- SQL/ConsecutiveNumbers.sql
+++ SQL/ConsecutiveNumbers.sql
@@ -1,7 +1,3 @@
-SELECT DISTINCT num AS ConsecutiveNums
-FROM
-    (
-        SELECT num, LEAD(num, 1) OVER() AS LD, LAG(num, 1) OVER() AS LG
-        FROM Logs
-    ) AS T
-WHERE LD = num AND LG = num
+SELECT DISTINCT l1.Num AS ConsecutiveNums
+FROM Logs l1, Logs l2, Logs l3
+WHERE l1.Id = l2.Id - 1 AND l2.Id = l3.Id - 1 AND l1.Num = l2.Num AND l2.Num = l3.Num;

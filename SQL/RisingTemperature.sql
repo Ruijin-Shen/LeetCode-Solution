1c5dab0382ceddef21f2349fc5a395570ad869e1
Ruijin-Shen <ruijinshen@gmail.com>
2024-06-20 20:58:12 +0800

    Solution to 197. Rising Temperature

--- SQL/RisingTemperature.sql
+++ SQL/RisingTemperature.sql
@@ -2,8 +2,16 @@ SELECT T.id
 FROM Weather as T join Weather as P ON DATEDIFF(T.recordDate, P.recordDate) = 1 AND T.temperature > P.temperature;
 
 
-/*
 SELECT T.id
 FROM Weather as T, Weather as P
 WHERE T.temperature > P.temperature AND T.recordDate = (P.recordDate + Interval '1' DAY);
-*/
+
+WITH PreviousWeatherData AS (
+    SELECT id, recordDate, temperature,
+           LAG(temperature, 1)OVER(ORDER BY recordDate) AS PreviousTemperature,
+           LAG(recordDate, 1)OVER(ORDER BY recordDate) AS PreviousRecordDate
+    FROM Weather)
+
+SELECT id
+FROM PreviousWeatherData
+WHERE temperature > PreviousTemperature AND recordDate = (PreviousRecordDate + INTERVAL 1 DAY);

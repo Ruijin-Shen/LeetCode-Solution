46f8586ef5178669b565ad4d2778f11e8e0c5c11
Ruijin-Shen <ruijinshen@gmail.com>
2024-06-20 22:42:06 +0800

    Solution to 570. Managers with at Least 5 Direct Reports.

--- SQL/ManagersWithAtLeast5DirectReports.sql
+++ SQL/ManagersWithAtLeast5DirectReports.sql
@@ -2,3 +2,11 @@ SELECT M.name
 FROM Employee AS E JOIN Employee AS M ON E.managerId = M.id
 GROUP BY M.id
 HAVING COUNT(E.id) >= 5;
+
+SELECT name
+FROM Employee
+WHERE id IN (
+    SELECT managerId
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5);

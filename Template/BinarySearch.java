6d9f8a2d7fe02d42b69f0f9e3c0d172aa287287b
Ruijin-Shen <ruijinshen@gmail.com>
2024-07-16 21:20:11 +0800

    Binary Search

--- Template/BinarySearch.java
+++ Template/BinarySearch.java
@@ -1,14 +1,10 @@
 package Template;
 
-import java.io.BufferedReader;
-import java.util.function.Predicate;
-
 public class BinarySearch {
     public static boolean check(int i){
         return true;
     }
 
-    // Find the minimum element.
     public static int binarySearchRight(int left, int right){
         while(left < right){
             int mid = left + right >> 1;
@@ -18,7 +14,6 @@ public class BinarySearch {
         return left;
     }
 
-    // Find the maximum element.
     public static int binarySearchLeft(int left, int right){
         while(left < right){
             int mid = left + right + 1 >> 1;

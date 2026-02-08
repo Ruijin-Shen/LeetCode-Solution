91e58d14ebded8a5558713650d9ab869e1591653
Ruijin Shen <ruijinshen@gmail.com>
2026-02-08 21:26:55 +0800

    Solution to 28. Find the Index Of the First Occurrence In a String

--- FindTheIndexOfTheFirstOccurrenceInAString.java
+++ FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,13 +1,10 @@
+import Template.KMP;
+
 public class FindTheIndexOfTheFirstOccurrenceInAString {
 }
 
 class Solution28KMP {
     public int strStr(String haystack, String needle) {
-        var result = KMP.kmp(haystack, needle);
-        if(result.size() == 0){
-            return -1;
-        }else{
-            return result.get(0);
-        }
+        return KMP.kmp(haystack, needle).stream().findFirst().orElse(-1);
     }
 }
